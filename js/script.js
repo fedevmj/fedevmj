c86798d671767c9b61c5280afedb506dbc0cdf1a
mj <dev.mjleemj@gmail.com>
2022-04-29 16:28:23 +0900

    favicon 추가, 여백조정, 헤더수정

--- js/script.js
+++ js/script.js
@@ -1,5 +1,48 @@
 window.onload = function(){
 
+    // 메뉴 이동
+    let gnb_a = $('.gnb > li > a');
+    let menu = ['#portfolio', '#aboutme', '#experiences', '#contact'];
+    let menu_position = [];
+
+    for(let i = 0; i < menu.length; i++){
+        let link = menu[i];
+        // '#portfolio', '#aboutme', '#experiences', '#contact'
+        menu_position[i] = $(link).offset().top;
+        // menu_position[i] -= 40;
+        // 641, 5532.234375, 7046.234375, 7936.234375
+    }
+
+    $.each(gnb_a, function (index, item) {
+        $(this).click(function (event) {
+            event.preventDefault();
+            link = $(this).attr('href');
+            let link_position = menu_position[index];
+            // console.log(link_position)
+
+            $('html').animate({
+                scrollTop: link_position
+            }, 1000)
+        })
+    });
+
+    // 헤더
+    let header = $('.header')
+    let seemywork_position = $('.seemywork p').offset().top;
+
+    $(window).scroll(function(){
+        let window_h = $(window).scrollTop();
+
+        if(window_h > seemywork_position){
+            header.addClass('header-active')
+        }else{
+            header.removeClass('header-active')
+        }
+    })
+
+
+
+
     const text = "Hi, I'm Lee Minjae,  \n a front-end developer who loves learning & challenging myself.";
     const text_space = document.querySelector('.text-box > h1');
 
