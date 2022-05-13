b9c6470ad6765f333829816ca37a6e243e2a7bc5
mj <dev.mjleemj@gmail.com>
2022-05-13 22:59:31 +0900

    로고추가, 헤더높이수정, 컨텐츠위치수정

--- js/script.js
+++ js/script.js
@@ -1,92 +1,92 @@
-window.onload = function(){
+window.onload = function () {
+  // 메뉴 이동
+  let gnb_a = $(".gnb > li > a");
+  let menu = ["#aboutme", "#portfolio", "#experiences", "#contact"];
+  let menu_position = [];
 
-    // 메뉴 이동
-    let gnb_a = $('.gnb > li > a');
-    let menu = ['#portfolio', '#aboutme', '#experiences', '#contact'];
-    let menu_position = [];
+  for (let i = 0; i < menu.length; i++) {
+    let link = menu[i];
+    // '#portfolio', '#aboutme', '#experiences', '#contact'
+    menu_position[i] = $(link).offset().top;
+    // menu_position[i] -= 40;
+    // 641, 5532.234375, 7046.234375, 7936.234375
+  }
 
-    for(let i = 0; i < menu.length; i++){
-        let link = menu[i];
-        // '#portfolio', '#aboutme', '#experiences', '#contact'
-        menu_position[i] = $(link).offset().top;
-        // menu_position[i] -= 40;
-        // 641, 5532.234375, 7046.234375, 7936.234375
-    }
+  $.each(gnb_a, function (index, item) {
+    $(this).click(function (event) {
+      event.preventDefault();
+      link = $(this).attr("href");
+      let link_position = menu_position[index];
+      // console.log(link_position)
 
-    $.each(gnb_a, function (index, item) {
-        $(this).click(function (event) {
-            event.preventDefault();
-            link = $(this).attr('href');
-            let link_position = menu_position[index];
-            // console.log(link_position)
-
-            $('html').animate({
-                scrollTop: link_position
-            }, 1000)
-        })
+      $("html").animate(
+        {
+          scrollTop: link_position,
+        },
+        1000
+      );
     });
+  });
 
-    // 헤더
-    let header = $('.header')
-    let text_position = $('.text-box').offset().top;
-    let window_h = $(window).scrollTop();
+  // 헤더
+  let header = $(".header");
+  let text_position = $(".text-box").offset().top;
+  let window_h = $(window).scrollTop();
 
+  window_h = $(window).scrollTop();
+  if (window_h > text_position) {
+    header.addClass("header-active");
+  } else {
+    header.removeClass("header-active");
+  }
+
+  // 스크롤했을 때
+
+  $(window).scroll(function () {
     window_h = $(window).scrollTop();
-    if(window_h > text_position){
-        header.addClass('header-active')
-    }else{
-        header.removeClass('header-active')
+
+    if (window_h > text_position) {
+      header.addClass("header-active");
+    } else {
+      header.removeClass("header-active");
+    }
+  });
+
+  const text =
+    "Hi, I'm Lee Minjae,  \n a front-end developer who loves learning & challenging myself.";
+  const text_space = document.querySelector(".text-box > h1");
+
+  let i = 0;
+  let text_timer;
+  let text_timer_delay;
+
+  // return;
+  let totalTxt = "";
+  function typing() {
+    let txt = text[i++];
+
+    if (txt === "\n") {
+      totalTxt += "<br>";
+    } else {
+      totalTxt += txt;
     }
 
-    // 스크롤했을 때 
+    text_space.innerHTML = totalTxt + '<span class="blink"></span>';
 
-    $(window).scroll(function(){
-        window_h = $(window).scrollTop();
-
-        if(window_h > text_position){
-            header.addClass('header-active')
-        }else{
-            header.removeClass('header-active')
-        }
-    })
-
-    const text = "Hi, I'm Lee Minjae,  \n a front-end developer who loves learning & challenging myself.";
-    const text_space = document.querySelector('.text-box > h1');
-
-    let i = 0;
-    let text_timer;
-    let text_timer_delay;
-
-    // return;
-    let totalTxt = '';
-    function typing(){
-        let txt = text[i++];
-        
-        if(txt === '\n') {
-            totalTxt += '<br>';
-        }else{
-            totalTxt += txt;
-        }
-
-        text_space.innerHTML = totalTxt + '<span class="blink"></span>';
-
-        if(i > text.length - 1){            
-            clearInterval(text_timer);
-            text_timer_delay = setInterval(delayTyping, 2000);
-        }
+    if (i > text.length - 1) {
+      clearInterval(text_timer);
+      text_timer_delay = setInterval(delayTyping, 2000);
     }
+  }
 
+  text_timer = setInterval(typing, 150);
+
+  function delayTyping() {
+    txt = "";
+    totalTxt = "";
+    text_space.innerHTML = "";
+    i = 0;
+    clearInterval(text_timer_delay);
     text_timer = setInterval(typing, 150);
-
-    function delayTyping() {
-        txt = '';
-        totalTxt = '';
-        text_space.innerHTML = '';
-        i = 0;
-        clearInterval(text_timer_delay);
-        text_timer = setInterval(typing, 150);
-    }
-
-
-
-}
+  }
+};
